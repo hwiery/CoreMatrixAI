729c84f133248caf4091bfecc7d7a8f6f84a9f28
hwiery <hwiery@naver.com>
2024-12-18 17:01:52 +0900

    feat: Add favicon configuration v1.0.10

---
+++ d
@@ -0,0 +1,15 @@
+  develop[m
+  feature/241209_0455_merge_test[m
+  feature/favicon[m
+  feature/new-work[m
+  feature/v1.0.3[m
+  feature/v1.0.6[m
+  feature/v1.0.7[m
+  main[m
+  master[m
+* [32mv.1.0.9[m
+  v1.0.0[m
+  v1.0.1-env[m
+  v1.0.2-env[m
+  v1.0.4[m
+  v1.0.8[m
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,25 @@
 import '../styles/globals.css';
 import Layout from '../components/Layout';
 import { LanguageProvider } from '../contexts/LanguageContext';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <LanguageProvider>
+      <Head>
+        <link 
+          rel="icon" 
+          type="image/png" 
+          sizes="32x32" 
+          href="/images/favicon-32x32.png"
+        />
+        <link 
+          rel="icon" 
+          type="image/png" 
+          sizes="16x16" 
+          href="/images/favicon-16x16.png"
+        />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
